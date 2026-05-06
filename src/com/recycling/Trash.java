685601445fde7dfd30fc55f2939526aae694318b
김서현 <snjksh486@gmail.com>
2026-05-06 09:44:18 +0900

    김서현님 담당

--- src/com/recycling/Trash.java
+++ src/com/recycling/Trash.java
@@ -1,1 +1,16 @@
-ㅁ
+public class Trash {
+    int x, y;
+    int size;
+    TrashType type;
+
+    public Trash(int x, int y, int size, TrashType type) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.type = type;
+    }
+
+    public void fall(int speed) {
+        y += speed;
+    }
+}
